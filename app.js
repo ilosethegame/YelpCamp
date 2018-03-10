85a00c0182fbebf1f29e377f4c33e3478a0dbead
Mark Maxwell <ilosethegame116@gmail.com>
2018-03-10 17:03:45 +0000

    add changed version

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp_v13");
+mongoose.connect("mongodb://localhost/yelp_camp_v13deploy");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
